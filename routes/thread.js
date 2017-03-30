dfe6eab853f38234c2de048cae11a3ea771f99a3
Warlon Zeng <wz634@nyu.edu>
2017-03-30 13:23:45 -0400

    dump sql, connecting mysql to cluster

--- routes/thread.js
+++ routes/thread.js
@@ -75,46 +75,6 @@ router.post('/NYU/:sub/:subid/:user/createThread', upload.single('file'), functi
 	});
 });
 
-function getPoints(client, id, res, done, query) {
-	client.query(query, [id], function(err, result) {
-		res.json(result[0]);
-	})
-}
-
-router.get('/voteThread/:user/:thread_id/:rating', function(req, res){
-	if(! req.session.hasOwnProperty(req.params.user)){
-		res.redirect('/');
-	}
-	var queryFind = 'SELECT username FROM thread_ratings WHERE thread_id = ? and username = ?';
-	var voteThread = 'INSERT INTO thread_ratings (thread_id, username, rating) VALUES(?, ?, ?)';
-	var updateVote = 'UPDATE thread_ratings SET rating = ? WHERE thread_id = ? and username = ?';
-	var updateThreadPoints = 'UPDATE thread SET points = points + ? WHERE thread_id = ? and username = ?'
-	var findPoints = 'SELECT points FROM thread WHERE id = ?';
-
-	console.log('hi');
-
-	pool.getConnection(function(err, client, done) {
-		client.query(queryFind, [req.params.thread_id, req.params.user], function(err, result) { // if did not vote
-			if (result.length === 0) {
-				client.query(voteThread, [req.params.thread_id, req.params.user, req.params.rating], function(err, result) { // vote
-					console.log('initial vote');
- 					getPoints(client, req.params.thread_id, res, done, findPoints);
- 					client.release();
-				});
-			}
-			if (result[0].rating != req.params.rating) { // make sure we only update the opposite 
-				client.query(updateVote, [req.params.rating, req.params.thread_id, req.params.user], function(err, result) {
-					client.query(updateThreadPoints, [req.params.rating, req.params.thread_id, req.params.user], function(err, result) {
-						console.log('update vote');
-						getPoints(client, req.params.thread_id, res, done, findPoints);
-						client.release();
-					});
-				});
-			}
-		});
-	});
-});
-
 router.get('/downloadFile/:thread_id', function(req, res){
 	var downloadFile = 'SELECT filename, data FROM file WHERE thread_id = ?';
 
