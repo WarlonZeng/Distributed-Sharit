957a086f1ceda5e763ad152243a9c940f51c264a
Warlon Zeng <wz634@nyu.edu>
2017-04-20 19:01:06 -0400

    working on backend db connection cloud mysql

--- backend/config/mysql/mysql_config.js
+++ backend/config/mysql/mysql_config.js
@@ -2,7 +2,7 @@ module.exports = {
 	user: 'root',
 	database: 'sharit',
 	password: '7183596771',
-	host: 'localhost',
+	host: '172.31.63.231',
 	connectionLimit : 100,
 	multipleStatements: true
 }
