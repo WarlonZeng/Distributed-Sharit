71903ecdd1809b3a7bb234e4e924d9e38b4a53e6
Warlon Zeng <wz634@nyu.edu>
2017-04-21 04:45:13 -0400

    fixed?

--- backend/routes/rating.js
+++ backend/routes/rating.js
@@ -41,7 +41,7 @@ router.post('/vote_thread/NYU', function(req, res) {
 				}
 			}
 			else {
-				client.query(insert_user_rating_and_thread_points, [req.body.thread_id, req.body.rating, req.body.username, req.body.rating, req.body.thread_id], function(err, result) {
+				client.query(insert_user_rating_and_thread_points, [req.body.thread_id, req.body.username, req.body.rating, req.body.rating, req.body.thread_id], function(err, result) {
 					client.query(find_thread_points, [req.body.thread_id], function(err, result) {
 						res.json({points: result[0]});
 					});
@@ -80,7 +80,7 @@ router.post('/vote_comment/NYU', function(req, res) {
 				}
 			}
 			else {
-				client.query(insert_user_rating_and_comment_points, [req.body.comment_id, req.body.rating, req.body.username, req.body.rating, req.body.comment_id], function(err, result) {
+				client.query(insert_user_rating_and_comment_points, [req.body.comment_id, req.body.username, req.body.rating, req.body.rating, req.body.comment_id], function(err, result) {
 					client.query(find_comment_points, [req.body.comment_id], function(err, result) {
 						res.json({points: result[0]});
 					});
